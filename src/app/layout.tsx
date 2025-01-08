5f30d802935b704f95b454f0688bd49139b492a8
hhannahhwang <hhannah.k.hwang@gmail.com>
2025-01-07 16:32:44 -0800

    updated fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,34 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 import { ReactQueryClientProvider } from "@/utils/react-query";
+import { Jost, Roboto, Questrial, Hanken_Grotesk } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const jost = Jost({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--jost-font",
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--roboto-font",
+});
+
+const questrial = Questrial({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+  variable: "--questrial-font",
+});
+
+const hanken_grotesk = Hanken_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--hanken_grotesk-font",
+});
 
 export const metadata = {
   title: "UCR Quant",
@@ -16,7 +42,9 @@ type LayoutProps = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${jost.variable} ${roboto.variable} ${questrial.variable} ${hanken_grotesk.variable}`}
+      >
         <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jost: ["var(--jost-font)"],
+        roboto: ["var(--roboto-font)"],
+        questrial: ["var(--questrial-font)"],
+        hanken_grotesk: ["var(--hanken_grotesk-font)"],
+      },
+    },
   },
   plugins: [],
 };

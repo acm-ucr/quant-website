01fa910ee11680595f912f988683f0597e2553f0
javi <javi@ucr-secure-43-10-13-221-198.wnet.ucr.edu>
2025-01-08 10:30:34 -0800

    Add Pages to App

---
+++ src/app/events/page.tsx
@@ -0,0 +1,9 @@
+const Events = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      This is the Events Page
+    </div>
+  );
+};
+
+export default Events;

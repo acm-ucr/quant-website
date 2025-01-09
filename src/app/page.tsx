bfc952dcbce4a30c661c345fdd8161d86113cad6
SM Kamal <smkamal1005@gmail.com>
2025-01-09 14:13:37 -0800

    Worked on ExampleComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Quant from "../components/example";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Quant />
+      <Quant text1="Quant" text2="Website" />
+      <Quant text1="Quant" text2="Wbsite" />
     </div>
   );
 };
--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Quant = () => {
-  return <div>Quant Website!</div>;
+const Quant = (props: { text1: string; text2: string }) => {
+  return (
+    <div className="bg-green-200 text-2xl">
+      <p className="text-3xl">{props.text1}</p>
+      <p className="text-5xl">{props.text2}</p>
+    </div>
+  );
 };
 
 export default Quant;

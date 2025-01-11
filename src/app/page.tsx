648c6ea36abe68b808f3f7b0b538f56038f00476
SafeDuck <schopra579@gmail.com>
2025-01-10 20:22:10 -0800

    Add example component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Quant from "../components/example";
+import Example from "../components/example";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Quant />
+      <Example text1="Adjust" text2="these" text3="Props" />
     </div>
   );
 };

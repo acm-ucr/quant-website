94af0a21d63412ffed02779f8739101a1c3f065f
Chabbies <fyythai@gmail.com>
2025-01-18 21:25:44 -0800

    Modified the home page to match the Example Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Quant from "../components/example";
+import Example from "../components/example";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Quant />
+      <Example text1="Some" text2="Props" text3="Here" />
     </div>
   );
 };
--- src/components/example.tsx
+++ src/components/example.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Columns } from "lucide-react";
 
-const Quant = () => {
-  return <div>Quant Website!</div>;
+const Example = (props: { text1: string; text2: string; text3: string }) => {
+  return (
+    <div className="flex w-2/3 flex-col items-center gap-5 rounded-md bg-red-200 p-8">
+      <div className="w-1/2 bg-blue-300 p-5 text-center">{props.text1}</div>
+      <div className="flex w-1/2 flex-row gap-7">
+        <div className="w-full rounded-full bg-blue-500 p-5 text-center">
+          {props.text2}
+        </div>
+        <div className="w-full rounded-full bg-blue-600 p-5 text-center">
+          {props.text2}
+        </div>
+      </div>
+      <div className="w-1/2 bg-blue-700 p-5 text-center text-2xl text-white">
+        {props.text3}
+      </div>
+    </div>
+  );
 };
 
-export default Quant;
+export default Example;

2297f9a41c7f5c4e71d14d95fe6319c49d2e9508
Ampherost <Ampherost235@gmail.com>
2025-01-22 14:25:57 -0800

    Final version v1.2

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image";
 import leftLine from "/public/header/left-line.svg";
 import rightLine from "/public/header/right-line.svg";
 
@@ -7,12 +7,12 @@ const Header = (props: { title: string }) => {
     <div className="w-full bg-gradient-to-r from-blue-900 to-blue-800 py-5">
       <div className="mx-auto flex w-3/4 items-center justify-center gap-20">
         <Image
-           src={leftLine}
-           alt="left-line"
-           aria-hidden="true"
-           width={360} 
-           height={20}  
-           className="flex h-full"
+          src={leftLine}
+          alt="left-line"
+          aria-hidden="true"
+          width={360}
+          height={20}
+          className="flex h-full"
         />
 
         <h1 className="text-center font-hanken text-5xl font-bold text-white">
@@ -23,8 +23,8 @@ const Header = (props: { title: string }) => {
           src={rightLine}
           alt="right-line"
           aria-hidden="true"
-          width={360} 
-          height={20}  
+          width={360}
+          height={20}
           className="flex h-full"
         />
       </div>
@@ -33,4 +33,3 @@ const Header = (props: { title: string }) => {
 };
 
 export default Header;
-

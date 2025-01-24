a8deffcb435aa07768a4ae4fd7daa87c88e96776
ACM UCR <fake@domain.com>
2025-01-24 04:13:58 +0000

    convert images to webp

--- src/data/techstack.ts
+++ src/data/techstack.ts
@@ -1,11 +1,11 @@
 const techStack = [
-  { image: "/techstack/python.png", link: "https://www.python.org" },
-  { image: "/techstack/pandas.png", link: "https://pandas.pydata.org/" },
-  { image: "techstack/stata.png", link: "https://www.stata.com/" },
-  { image: "/techstack/tailwind.png", link: "https://tailwindcss.com" },
-  { image: "/techstack/flask.png", link: "https://flask.palletsprojects.com" },
-  { image: "/techstack/ocaml.png", link: "https://ocaml.org/" },
-  { image: "/techstack/numpy.png", link: "https://numpy.org" },
+  { image: "/techstack/python.webp", link: "https://www.python.org" },
+  { image: "/techstack/pandas.webp", link: "https://pandas.pydata.org/" },
+  { image: "techstack/stata.webp", link: "https://www.stata.com/" },
+  { image: "/techstack/tailwind.webp", link: "https://tailwindcss.com" },
+  { image: "/techstack/flask.webp", link: "https://flask.palletsprojects.com" },
+  { image: "/techstack/ocaml.webp", link: "https://ocaml.org/" },
+  { image: "/techstack/numpy.webp", link: "https://numpy.org" },
 ];
 
 export default techStack;

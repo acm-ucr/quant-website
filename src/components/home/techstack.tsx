21820bc9380d7bdd463f67b20dc7e0f2419319df
WinThant16 <winthant1601@gmail.com>
2025-01-26 06:36:54 -0800

    Changed <a> and <img> to now use <Link> and <Image>

--- src/components/home/techstack.tsx
+++ src/components/home/techstack.tsx
@@ -1,21 +1,32 @@
 import techStack from "@/data/techstack";
+import Image from "next/image";
+import Link from "next/link";
 
 const TechStack = () => {
   return (
     <div className="flex h-full w-full flex-col flex-wrap items-center justify-center gap-10">
+      {/* Title */}
       <div className="font-hanken text-4xl font-bold text-white underline">
         Our Tech Stack
       </div>
-      <div className="flex flex-row flex-wrap items-center justify-center gap-20 pb-20">
+
+      {/* Tech Stack Icons */}
+      <div className="flex flex-row flex-wrap items-center justify-center gap-10 pb-20">
         {techStack.map((tech, index) => (
-          <a
+          <Link
             key={index}
             href={tech.link}
             target="_blank"
             rel="noopener noreferrer"
+            className="h-[10%] w-[10%] p-[1%]"
           >
-            <img src={tech.image} alt="Tech Logo" className="h-32 w-32" />
-          </a>
+            <Image
+              src={tech.image}
+              alt={`Tech Logo`}
+              width={400}
+              height={400}
+            />
+          </Link>
         ))}
       </div>
     </div>
--- src/data/techstack.ts
+++ src/data/techstack.ts
@@ -1,7 +1,7 @@
 const techStack = [
   { image: "/techstack/python.webp", link: "https://www.python.org" },
   { image: "/techstack/pandas.webp", link: "https://pandas.pydata.org/" },
-  { image: "techstack/stata.webp", link: "https://www.stata.com/" },
+  { image: "/techstack/stata.webp", link: "https://www.stata.com/" },
   { image: "/techstack/tailwind.webp", link: "https://tailwindcss.com" },
   { image: "/techstack/flask.webp", link: "https://flask.palletsprojects.com" },
   { image: "/techstack/ocaml.webp", link: "https://ocaml.org/" },

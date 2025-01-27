1ca862e977f453abbf6b9112fdbb8d6ba64d6eea
SM Kamal <smkamal1005@gmail.com>
2025-01-27 00:23:48 -0800

    Fixed PR comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Project from "../components/project";
-
+import img from "../components/exampleACM.png";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen flex-wrap items-center justify-center bg-indigo-950">
       <Project
         image_alt="Image goes here"
-        image_src="Image"
+        image_src={img.src}
         name="Project Name"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan urna nec libero fringilla, ac fermentum purus tempus. Sed et nisi velit. Sed sit amet justo magna. Vivamus malesuada, odio quis consectetur convallis, nulla erat feugiat risus, ut ultricies velit ex eu odio. Cras aliquet euismod ipsum nec rutrum. Integer in lorem id ipsum gravida convallis. Sed nec risus vel purus varius eleifend. Nunc id molestie nisl. Sed ac ante quis nisl tincidunt feugiat. Maecenas pharetra libero ut nunc vulputate, in mattis velit ultricies. Maecenas tincidunt magna a nisl bibendum, nec auctor nunc iaculis."
       />
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 const Project = (props: {
   image_src: string;
   image_alt: string;
@@ -5,16 +6,21 @@ const Project = (props: {
   description: string;
 }) => {
   return (
-    <div className="w-7/8 flex h-auto flex-col text-lg">
-      <div className="w-7/8 flex h-auto flex-wrap items-center justify-center gap-24 p-8 text-white">
-        <div className="flex h-44 w-44 items-center justify-center rounded-full bg-[#94C7ED]">
-          <img src={props.image_src} alt={props.image_alt} />
+    <div className="flex h-2/6 w-8/12 flex-col text-lg">
+      <div className="flex flex-row items-center justify-center gap-24 p-8 text-white">
+        <div className="flex h-1/2 w-1/6 items-center justify-center rounded-full bg-[#94C7ED]">
+          <Image
+            src={props.image_src}
+            alt={props.image_alt}
+            width={70}
+            height={70}
+          />
         </div>
         <div className="flex flex-col">
-          <div className="h-20 w-60 font-questrial text-4xl font-bold">
+          <div className="p-3 font-questrial text-4xl font-bold">
             {props.name}
           </div>
-          <div className="h-44 max-w-5xl font-roboto text-lg">
+          <div className="max-w-5xl p-3 font-roboto text-lg">
             {props.description}
           </div>
         </div>

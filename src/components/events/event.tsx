29adc378b917d3c419c99760f0f0783fa9ab2cfc
Shivani Nandakumar <snand021@ucr.edu>
2025-01-28 00:21:41 -0800

    removed hard coded vals

--- src/components/events/event.tsx
+++ src/components/events/event.tsx
@@ -7,7 +7,7 @@ const EventCard = (props: {
   description: string;
 }) => {
   return (
-    <div className="flex h-[251px] w-[1184px] items-center rounded-3xl bg-[#FFFFFF61]">
+    <div className="flex h-1/4 w-3/4 items-center rounded-3xl bg-[#FFFFFF61]">
       <div className="flex h-full w-1/5 flex-col items-center justify-center">
         <div className="text-center font-questrial text-xl text-[#A7D6FD]">
           {props.day}
@@ -20,15 +20,15 @@ const EventCard = (props: {
         <div className="justify-start pl-[30px] pt-[45px] font-questrial text-4xl font-medium text-[#FFFFFF]">
           {props.title}
         </div>
-        <div className="flex h-[45px] w-[415px] flex-row gap-20 pl-[30px]">
-          <div className="h-[32px] w-[107px] pl-[40px] font-roboto text-lg text-[#A7D6FD]">
+        <div className="flex h-2/8 w-1/2 flex-row gap-20 pl-[30px]">
+          <div className="h-1/8 w-1/5 pl-[40px] font-roboto text-lg text-[#A7D6FD]">
             {props.location}
           </div>
-          <div className="h-[32px] w-[107px] pl-[40px] font-roboto text-lg text-[#A7D6FD]">
+          <div className="h-7/8 w-1/2 pl-[40px] font-roboto text-lg text-[#A7D6FD]">
             {props.time}
           </div>
         </div>
-        <div className="h-[72px] w-[914px] pl-[30px] font-roboto text-lg text-[#BDBDBD]">
+        <div className="h-full w-5/6 pl-[30px] font-roboto text-lg text-[#BDBDBD]">
           {props.description}
         </div>
       </div>

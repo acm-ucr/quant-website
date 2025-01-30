8b3fc48680e9ebcce9ffa9a0ba91ee7aee93a3ea
j1munoz <jimmymunoz397@gmail.com>
2025-01-30 15:20:01 -0800

    Fixed fomratting

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,21 +4,30 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-      <div className="mt-[20%] mb-[20%] relative w-full flex flex-col justify-center items-center content-center">
-        <Image src={GRID} alt="grid" className="w-4/5 -z-10"/>
-          <div className="absolute top-[47%] left-1/2 flex flex-col items-center justify-center transform -translate-x-1/2 -translate-y-1/2 gap-5 w-4/5">
-            <div className="text-center flex flex-col items-center justify-center">
-              <h1 className="text-quant-white text-6xl md:text-[150px] leading-none font-mono">404</h1>
-              <div className="border-b-4 w-2/3 border-[#5BC6ED]"></div>
-            </div>
-            <div>
-              <h2 className="text-3xl leading-none font-questrial text-quant-white font-bold">PAGE NOT FOUND</h2>
-            </div>
-            <div className="p-9">
-              <Link href="/" className="bg-[#87C3F2] rounded-full text-lg sm:text-xl md:text-2xl border-2 border-white px-6 py-2 font-hanken text-quant-navy font-bold">Back to Home</Link>
-            </div>
-          </div>
+    <div className="relative mb-[20%] mt-[20%] flex w-full flex-col content-center items-center justify-center">
+      <Image src={GRID} alt="grid" className="-z-10 w-4/5" />
+      <div className="absolute left-1/2 top-[47%] flex w-4/5 -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center gap-5">
+        <div className="flex flex-col items-center justify-center text-center">
+          <h1 className="font-mono text-6xl leading-none text-quant-white md:text-[150px]">
+            404
+          </h1>
+          <div className="w-2/3 border-b-4 border-[#5BC6ED]"></div>
+        </div>
+        <div>
+          <h2 className="font-questrial text-3xl font-bold leading-none text-quant-white">
+            PAGE NOT FOUND
+          </h2>
+        </div>
+        <div className="p-9">
+          <Link
+            href="/"
+            className="rounded-full border-2 border-white bg-[#87C3F2] px-6 py-2 font-hanken text-lg font-bold text-quant-navy sm:text-xl md:text-2xl"
+          >
+            Back to Home
+          </Link>
+        </div>
       </div>
+    </div>
   );
 };
 

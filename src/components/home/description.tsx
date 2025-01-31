af902ac5d7959a2208ee2da2574b46d76095701a
Jacob <110055706+frog187man@users.noreply.github.com>
2025-01-30 17:51:30 -0800

    responsified description component to match figma

--- src/components/home/description.tsx
+++ src/components/home/description.tsx
@@ -8,28 +8,28 @@ interface DescriptionProps {
 
 const Description = ({ title, description }: DescriptionProps) => {
   return (
-    <div className="relative mx-10 flex h-fit max-w-7xl flex-col">
-      <div className="mb-10 px-10 font-hanken text-5xl font-bold text-white">
+    <div className="relative mx-10 flex h-fit max-w-7xl flex-col px-5 sm:px-10 md:px-20">
+      <div className="mb-5 pr-10 text-center font-hanken text-4xl font-bold text-white md:mb-10 md:text-left md:text-5xl">
         {title}
       </div>
-      <div className="max-h-64 w-fit overflow-auto pl-10 pr-10 font-roboto text-lg text-white">
+      <div className="mb-10 max-h-64 w-fit overflow-auto text-center font-roboto text-lg text-white md:mb-0 md:pr-12 md:text-left">
         {description}
       </div>
       <div className="flex w-full flex-row justify-end">
         <Image
           src={Chart}
           alt="description-image"
-          className="h-full max-h-28 w-full max-w-28 object-cover"
+          className="hidden h-full max-h-28 w-full max-w-28 object-cover md:block"
         />
       </div>
       <div
-        className="absolute bottom-4 left-0 h-0.5 bg-white"
+        className="left-18 absolute bottom-4 hidden h-0.5 bg-white md:block"
         style={{
-          width: "calc(100% - 120px)",
+          width: "calc(100% - 280px)",
         }}
       ></div>
       <div
-        className="absolute right-4 top-0 w-0.5 bg-white"
+        className="absolute right-24 top-0 hidden w-0.5 bg-white md:block"
         style={{
           height: "calc(100% - 120px)",
         }}

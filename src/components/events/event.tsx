944f10b1c3efe7886739f224a687393b39e6dc4f
Shivani Nandakumar <snand021@ucr.edu>
2025-01-31 17:32:05 -0800

    fixed all recommended changes for event component

--- src/components/events/event.tsx
+++ src/components/events/event.tsx
@@ -13,19 +13,19 @@ const EventCard = (props: {
   return (
     <div className="flex h-1/4 w-3/4 items-center rounded-3xl bg-[#FFFFFF61]">
       <div className="flex h-full w-1/5 flex-col items-center justify-center">
-        <div className="text-center font-questrial text-xl text-[#A7D6FD]">
+        <div className="text-center font-questrial text-xl text-quant-blue-100">
           {props.day}
         </div>
-        <div className="text-center font-questrial text-7xl text-[#C3D0DF]">
+        <div className="text-center font-questrial text-7xl text-quant-gray">
           {props.date}
         </div>
       </div>
       <div className="flex h-full w-4/5 flex-col">
-        <div className="justify-start pl-10 pt-10 font-questrial text-4xl font-medium text-[#FFFFFF]">
+        <div className="justify-start pl-10 pt-10 font-questrial text-4xl font-medium text-quant-white">
           {props.title}
         </div>
         <div className="h-2/8 flex w-1/2 flex-row gap-20 pl-8">
-          <div className="h-1/8 flex w-1/5 flex-row gap-2 pl-1 font-roboto text-lg text-[#A7D6FD]">
+          <div className="h-1/8 flex w-1/5 flex-row gap-2 pl-1 font-roboto text-lg text-quant-blue-100">
             <Image
               src={locationIcon}
               alt="Location Icon"
@@ -34,12 +34,12 @@ const EventCard = (props: {
             />
             {props.location}
           </div>
-          <div className="h-7/8 flex w-1/2 flex-row gap-2 pl-10 font-roboto text-lg text-[#A7D6FD]">
+          <div className="h-7/8 flex w-1/2 flex-row gap-2 pl-10 font-roboto text-lg text-quant-blue-100">
             <Image src={timeIcon} alt="Time Icon" width={24} height={24} />
             {props.time}
           </div>
         </div>
-        <div className="h-full w-5/6 pl-10 pt-2 font-roboto text-lg text-[#BDBDBD]">
+        <div className="h-full w-5/6 pl-10 pt-2 font-roboto text-lg text-quant-gray">
           {props.description}
         </div>
       </div>

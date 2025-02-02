b81805a6e27190e8c8da22a54d945f92c5890c3e
SM Kamal <smkamal1005@gmail.com>
2025-02-02 13:11:57 -0800

    File management and updated hard coded css to just tailwind

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
-import image_src from "../../component/exampleACM.svg";
+import image_src from "@/public/projects/exampleACM.svg";
 const Project = (props: {
-
   image_alt: string;
   name: string;
   description: string;
@@ -10,12 +9,7 @@ const Project = (props: {
     <div className="flex h-2/6 w-8/12 flex-col text-lg">
       <div className="flex flex-row items-center justify-center gap-24 p-8 text-white">
         <div className="flex h-1/2 w-1/6 items-center justify-center rounded-full bg-[#94C7ED]">
-          <Image
-            src={image_src}
-            alt={props.image_alt}
-            width={70}
-            height={70}
-          />
+          <Image className="h-20 w-20" src={image_src} alt={props.image_alt} />
         </div>
         <div className="flex flex-col">
           <div className="p-3 font-questrial text-4xl font-bold">

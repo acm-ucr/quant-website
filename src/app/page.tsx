1ab556ff0fcd3d38eadcd4fb52edcb9a5ae4e3e8
SM Kamal <smkamal1005@gmail.com>
2025-02-02 10:11:12 -0800

    Responsify Project Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import Project from "../components/project";
-import img from "../components/exampleACM.png";
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen flex-wrap items-center justify-center bg-indigo-950">
-      <Project
-        image_alt="Image goes here"
-        image_src={img.src}
-        name="Project Name"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan urna nec libero fringilla, ac fermentum purus tempus. Sed et nisi velit. Sed sit amet justo magna. Vivamus malesuada, odio quis consectetur convallis, nulla erat feugiat risus, ut ultricies velit ex eu odio. Cras aliquet euismod ipsum nec rutrum. Integer in lorem id ipsum gravida convallis. Sed nec risus vel purus varius eleifend. Nunc id molestie nisl. Sed ac ante quis nisl tincidunt feugiat. Maecenas pharetra libero ut nunc vulputate, in mattis velit ultricies. Maecenas tincidunt magna a nisl bibendum, nec auctor nunc iaculis."
-      />
+    <div>
+      <head>
+        {" "}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />{" "}
+      </head>
+      <h1>Home</h1>
+      <p>Welcome to the Home page</p>
     </div>
   );
 };
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,31 +1,30 @@
 import Image from "next/image";
+import image_src from "@/public/projects/exampleACM.svg";
 const Project = (props: {
-  image_src: string;
   image_alt: string;
   name: string;
   description: string;
 }) => {
   return (
-    <div className="flex h-2/6 w-8/12 flex-col text-lg">
-      <div className="flex flex-row items-center justify-center gap-24 p-8 text-white">
-        <div className="flex h-1/2 w-1/6 items-center justify-center rounded-full bg-[#94C7ED]">
+    <div className="min-h-fit w-96 rounded-lg text-sm max-lg:border-[1px] max-lg:border-[#ADC0EB] max-lg:bg-[#CAE7FF69] lg:flex lg:h-2/6 lg:w-9/12 lg:flex-col lg:text-lg">
+      <div className="flex flex-col items-center justify-center p-4 text-white lg:flex-row lg:items-center lg:justify-center lg:gap-24 lg:p-8 lg:text-white">
+        <div className="flex h-16 w-16 items-center justify-center rounded-full bg-[#94C7ED] lg:flex lg:min-h-44 lg:min-w-44 lg:items-center lg:justify-center lg:rounded-full">
           <Image
-            src={props.image_src}
+            className="w-24 max-lg:w-10"
+            src={image_src}
             alt={props.image_alt}
-            width={70}
-            height={70}
           />
         </div>
-        <div className="flex flex-col">
-          <div className="p-3 font-questrial text-4xl font-bold">
+        <div className="flex flex-col justify-center max-lg:items-center">
+          <div className="p-3 font-questrial text-lg font-bold lg:text-4xl lg:font-bold">
             {props.name}
           </div>
-          <div className="max-w-5xl p-3 font-roboto text-lg">
+          <div className="font-roboto text-sm lg:max-w-5xl lg:p-3 lg:font-roboto lg:text-lg">
             {props.description}
           </div>
         </div>
       </div>
-      <div className="w-7/8 border-2"></div>
+      <div className="lg:w-7/8 lg:border-2"></div>
     </div>
   );
 };

c9a2dc6c64380ae5c92c98024daccb3054734b01
Ampherost <Ampherost235@gmail.com>
2025-02-05 12:56:30 -0800

    Populate pages w headers pt1

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,16 +1,22 @@
 import EventCard from "@/components/events/event";
+import Header from "../../components/header";
 
 const Events = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-blue-950">
-      <EventCard
-        day="Mon"
-        date="09"
-        title="General Meeting"
-        location="Location"
-        time="3 pm"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan urna nec libero fringilla, ac fermentum purus tempus. Sed et nisi velit."
-      />
+    <div className="min-h-screen bg-blue-950">
+      {/* Header with the title "Upcoming Events" */}
+      <Header title="Upcoming Events" />
+      
+      <div className="flex h-full w-full items-center justify-center">
+        <EventCard
+          day="Mon"
+          date="09"
+          title="General Meeting"
+          location="Location"
+          time="3 pm"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan urna nec libero fringilla, ac fermentum purus tempus. Sed et nisi velit."
+        />
+      </div>
     </div>
   );
 };

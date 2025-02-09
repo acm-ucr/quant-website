e4492de15263565d06f67705a0647c3431dea12a
Shivani Nandakumar <snand021@ucr.edu>
2025-02-08 22:28:02 -0800

    changes to sm

--- src/components/events/event.tsx
+++ src/components/events/event.tsx
@@ -11,20 +11,20 @@ const EventCard = (props: {
   description: string;
 }) => {
   return (
-    <div className="flex sm:h-1/3 sm:w-4/5 lg:h-1/3 lg:w-3/4 items-center rounded-3xl bg-[#FFFFFF61]">
-      <div className="flex sm:h-full sm:w-4/5 lg:h-full lg:w-1/5 flex-col sm:justify-start sm:items-center lg:items-center lg:justify-center sm:pt-7 lg:pt-0">
-        <div className="sm:text-left lg:text-center font-questrial sm:text-2xl lg:text-lg text-quant-blue-100">
-          {props.day} 
+    <div className="flex items-center rounded-3xl bg-[#FFFFFF61] sm:h-1/3 sm:w-4/5 lg:h-1/3 lg:w-3/4">
+      <div className="flex flex-col sm:h-full sm:w-4/5 sm:items-center sm:justify-start sm:pt-7 lg:h-full lg:w-1/5 lg:items-center lg:justify-center lg:pt-0">
+        <div className="font-questrial text-quant-blue-100 sm:text-left sm:text-2xl lg:text-center lg:text-lg">
+          {props.day}
         </div>
-        <div className="sm:text-left lg:text-center font-questrial sm:text-6xl sm:font-bold lg:text-7xl text-quant-white">
+        <div className="font-questrial text-quant-white sm:text-left sm:text-6xl sm:font-bold lg:text-center lg:text-7xl">
           {props.date}
         </div>
       </div>
-      <div className="flex sm:h-full lg:h-full lg:w-4/5 flex-col">
-        <div className="justify-start lg:pl-10 pt-10 font-questrial sm:text-3xl lg:text-4xl font-medium text-quant-white">
+      <div className="flex flex-col sm:h-full lg:h-full lg:w-4/5">
+        <div className="justify-start pt-10 font-questrial font-medium text-quant-white sm:text-3xl lg:pl-10 lg:text-4xl">
           {props.title}
         </div>
-        <div className="lg:h-2/8 flex lg:w-1/2 flex-row gap-20 lg:pl-8">
+        <div className="lg:h-2/8 flex flex-row gap-20 lg:w-1/2 lg:pl-8">
           <div className="h-1/8 flex w-1/5 flex-row gap-2 pl-1 font-roboto text-lg text-quant-blue-100">
             <Image
               src={locationIcon}
@@ -46,7 +46,7 @@ const EventCard = (props: {
             {props.time}
           </div>
         </div>
-        <div className="sm:h-full sm:pt-3 sm:text-base sm:w-5/7 lg:h-full lg:w-5/6 lg:pl-10 lg:pt-2 font-roboto text-lg text-quant-gray">
+        <div className="sm:w-5/7 font-roboto text-lg text-quant-gray sm:h-full sm:pt-3 sm:text-base lg:h-full lg:w-5/6 lg:pl-10 lg:pt-2">
           {props.description}
         </div>
       </div>

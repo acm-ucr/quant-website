fe1bedb9bafd47d910f7a664c8f32d165e1c3c5d
SM Kamal <smkamal1005@gmail.com>
2025-02-27 22:29:49 -0800

    Refactored code (props)

--- src/components/events/event.tsx
+++ src/components/events/event.tsx
@@ -3,7 +3,14 @@ import LocationIcon from "@/public/icons/LocationIconBlue.svg";
 import TimeIcon from "@/public/icons/timeIcon.svg";
 import TimeIconMobile from "@/public/icons/TimeIconMobile.svg";
 
-const EventCard = (props: {
+const EventCard = ({
+  day,
+  date,
+  title,
+  location,
+  time,
+  description,
+}: {
   day: string;
   date: string;
   title: string;
@@ -15,25 +22,25 @@ const EventCard = (props: {
     <div className="flex w-11/12 items-center gap-6 rounded-3xl bg-[#FFFFFF61] p-4 sm:h-1/3 sm:w-4/5 sm:pb-12 lg:h-1/3 lg:w-3/4">
       <div className="mb-20 hidden sm:mb-0 sm:flex sm:h-full sm:w-4/5 sm:flex-col sm:items-center sm:justify-start sm:pt-7 lg:h-full lg:w-1/5 lg:items-center lg:justify-center lg:pt-3">
         <div className="text-center font-questrial text-base uppercase text-[#DDF0FE] sm:text-left sm:text-2xl sm:text-quant-blue-100 lg:text-center lg:text-xl">
-          {props.day}
+          {day}
         </div>
         <div className="-mt-2 font-questrial text-4xl font-bold text-quant-white sm:-mt-0 sm:text-left sm:text-6xl sm:font-normal lg:text-center lg:text-7xl">
-          {props.date}
+          {date}
         </div>
       </div>
       <div className="flex flex-col sm:h-full lg:h-full lg:w-4/5">
         <div className="flex gap-5">
           <div className="sm:hidden">
             <div className="text-center font-questrial text-base uppercase text-[#DDF0FE]">
-              {props.day}
+              {day}
             </div>
             <div className="-mt-2 font-questrial text-4xl font-bold text-[#E8EDF3]">
-              {props.date}
+              {date}
             </div>
           </div>
           <div className="sm:w-2/3">
             <div className="justify-start pb-1 font-questrial text-xl font-medium text-quant-white sm:pb-0 sm:pt-10 sm:text-3xl lg:pl-10 lg:text-4xl">
-              {props.title}
+              {title}
             </div>
             <div className="lg:h-2/8 mt-1 flex flex-row gap-3 sm:mt-0 sm:gap-20 lg:w-1/2 lg:pl-8">
               <div className="h-1/8 flex w-1/5 flex-row gap-2 pl-1 font-roboto text-quant-blue-100 sm:text-lg">
@@ -45,7 +52,7 @@ const EventCard = (props: {
                   className="h-3/5 sm:mt-1 sm:h-4/5"
                 />
                 <div className="-translate-y-[1px] text-xs sm:-translate-y-0 sm:text-lg">
-                  {props.location}
+                  {location}
                 </div>
               </div>
               <div className="h-7/8 flex flex-row gap-2 pl-10 font-roboto text-sm text-quant-blue-100 sm:translate-x-12 sm:text-lg md:-translate-x-10 lg:-translate-x-0">
@@ -64,17 +71,17 @@ const EventCard = (props: {
                   className="hidden sm:block"
                 />
                 <div className="-translate-x-5 -translate-y-[1px] text-xs sm:-translate-x-0 sm:-translate-y-0 sm:text-lg">
-                  {props.time}
+                  {time}
                 </div>
               </div>
             </div>
             <div className="pt-2 text-xs text-[#BDBDBD] sm:hidden">
-              {props.description}
+              {description}
             </div>
           </div>
         </div>
         <div className="sm:w-5/7 hidden pt-3 font-roboto text-xs text-quant-gray sm:flex sm:h-full sm:pt-3 sm:text-lg lg:h-full lg:w-5/6 lg:pl-10 lg:pt-2">
-          {props.description}
+          {description}
         </div>
       </div>
     </div>
--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -4,19 +4,26 @@ import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
-interface ProjectProps {
+type ProjectProps = {
   image_src: StaticImageData;
   image_alt: string;
   name: string;
   description: string;
   delay?: number;
   link: string;
-}
+};
 
-const Project = (props: ProjectProps) => {
+const Project = ({
+  image_src,
+  image_alt,
+  name,
+  description,
+  delay = 0,
+  link,
+}: ProjectProps) => {
   const projectVariants = {
     hidden: { opacity: 0, y: 20 },
-    visible: (delay: number = 0) => ({
+    visible: (delay: number) => ({
       opacity: 1,
       y: 0,
       transition: { duration: 0.8, delay },
@@ -25,7 +32,7 @@ const Project = (props: ProjectProps) => {
 
   return (
     <motion.div
-      custom={props.delay || 0}
+      custom={delay || 0}
       variants={projectVariants}
       initial="hidden"
       whileInView="visible"
@@ -34,17 +41,13 @@ const Project = (props: ProjectProps) => {
     >
       <div className="flex flex-col items-center justify-center p-4 text-white lg:flex-row lg:items-center lg:justify-center lg:gap-24 lg:p-8 lg:text-white">
         <div className="flex h-16 w-16 items-center justify-center rounded-full bg-quant-blue-200 lg:flex lg:min-h-44 lg:min-w-48 lg:items-center lg:justify-center lg:rounded-full">
-          <Image
-            className="w-20 max-lg:w-10"
-            src={props.image_src}
-            alt={props.image_alt}
-          />
+          <Image className="w-20 max-lg:w-10" src={image_src} alt={image_alt} />
         </div>
         <div className="flex flex-col justify-center max-lg:items-center">
           <div className="flex flex-row items-center gap-2 p-3 font-questrial text-lg font-bold lg:gap-3 lg:text-4xl lg:font-bold">
-            {props.name}
+            {name}
             <Link
-              href={props.link}
+              href={link}
               target="_blank"
               rel="noopener noreferrer"
               className="transition-transform duration-300 hover:scale-110"
@@ -53,7 +56,7 @@ const Project = (props: ProjectProps) => {
             </Link>
           </div>
           <div className="text-center font-roboto text-sm lg:max-w-5xl lg:p-3 lg:text-left lg:font-roboto lg:text-lg">
-            {props.description}
+            {description}
           </div>
         </div>
       </div>

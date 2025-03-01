84f1badb67d0507eab2062801860cafa3ba7bcde
Yoshinao <yoshinaoshimono@gmail.com>
2025-02-28 17:18:59 -0800

    animate homepage

--- src/components/home/description.tsx
+++ src/components/home/description.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import { StaticImageData } from "next/image";
+import { motion } from "framer-motion";
 
 interface DescriptionProps {
   title: string;
@@ -7,15 +9,42 @@ interface DescriptionProps {
   image: StaticImageData;
 }
 
+const animationRight = {
+  hidden: { opacity: 0, x: -50 },
+  show: {
+    opacity: 1,
+    x: 0,
+  },
+};
+const animationUp = {
+  hidden: { opacity: 0, y: 50 },
+  show: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 const Description = ({ title, description, image }: DescriptionProps) => {
   return (
     <div className="relative mx-10 mb-10 flex h-fit max-w-7xl flex-col px-5 sm:px-10 md:px-20">
-      <div className="mb-5 pr-10 text-center font-hanken text-4xl font-bold text-white md:mb-10 md:text-left md:text-4xl">
+      <motion.div
+        className="mb-5 pr-10 text-center font-hanken text-4xl font-bold text-white md:mb-10 md:text-left md:text-4xl"
+        variants={animationRight}
+        transition={{ duration: 0.7, delay: 0.2 }}
+        initial="hidden"
+        whileInView="show"
+      >
         {title}
-      </div>
-      <div className="mb-10 w-fit overflow-auto text-center font-roboto text-lg text-white md:mb-0 md:pr-12 md:text-left">
+      </motion.div>
+      <motion.div
+        className="mb-10 w-fit overflow-auto text-center font-roboto text-lg text-white md:mb-0 md:pr-12 md:text-left"
+        variants={animationUp}
+        transition={{ duration: 0.7, delay: 0.2 }}
+        initial="hidden"
+        whileInView="show"
+      >
         {description}
-      </div>
+      </motion.div>
       <div className="flex w-full flex-row justify-end">
         <Image
           src={image}

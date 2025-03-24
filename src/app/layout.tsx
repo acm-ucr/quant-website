7088b44a56e7b44de58fa3fd1917a4f89feeb7b7
SafeDuck <schopra579@gmail.com>
2025-03-23 19:39:06 -0700

    chores: fix allignment and remove underlines

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,8 +62,13 @@ export default function RootLayout({ children }: LayoutProps) {
       <body
         className={`${jost.variable} ${roboto.variable} ${questrial.variable} ${hanken_grotesk.variable} ${istok_web.variable} relative flex flex-col justify-between bg-[#100E37]`}
       >
-        <div className="pointer-events-none absolute inset-0 -z-50 mt-[-200px] select-none overflow-hidden opacity-70">
-          <Image src={BackgroundEllipses} alt="" aria-hidden="true" />
+        <div className="pointer-events-none absolute inset-0 -z-50 mt-[5%] select-none overflow-hidden opacity-70">
+          <Image
+            src={BackgroundEllipses}
+            alt="Background Ellipses"
+            aria-hidden="true"
+            className="w-full"
+          />
         </div>
 
         <NavBar />
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import Projects from "@/components/project/projects";
 
 const ProjectsPage = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-8">
+    <div className="flex h-[80vh] w-full flex-col items-center gap-8">
       <Header>PROJECTS</Header>
       <Projects />
     </div>

28c14cb44da4eb0d0c0ffb83ce5d20110cb9b261
SafeDuck <schopra579@gmail.com>
2025-03-28 18:58:40 -0700

    Adjust landing text size

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -4,8 +4,8 @@ import CHART from "@/public/chart.svg";
 const Landing = () => {
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center">
-      <p className="absolute font-jost text-6xl font-bold md:text-9xl">
-        Welcome to Quant!
+      <p className="absolute font-jost text-5xl font-bold md:text-7xl xl:text-9xl">
+        Quant at UCR
       </p>
       <Image src={CHART} alt="Line chart" />
     </div>

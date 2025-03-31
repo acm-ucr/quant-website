c19c03c843f25df28cbcbac3a53e9592e8b5f658
hhannahhwang <hhannah.k.hwang@gmail.com>
2025-03-30 17:55:28 -0700

    fixed comments

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -1,12 +1,22 @@
+"use client";
 import Image from "next/image";
 import CHART from "@/public/chart.svg";
+import { motion } from "motion/react";
 
 const Landing = () => {
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center">
-      <p className="absolute font-jost text-6xl font-bold md:text-9xl">
+      <motion.div
+        transition={{
+          duration: 0.7,
+          delay: 0.2,
+        }}
+        initial={{ opacity: 0, scale: 0.8 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        className="absolute font-jost text-6xl font-bold md:text-9xl"
+      >
         Welcome to Quant!
-      </p>
+      </motion.div>
       <Image src={CHART} alt="Line chart" />
     </div>
   );
